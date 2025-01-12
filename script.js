2d8fe8b07dda3767fb6260da8b7140a905d3e510
Lucas <lucas10609@gmail.com>
2025-01-11 21:07:49 -0300

    feat(FunctionsalvarMensagem):'Added function salvarMensagem'

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-function 
+function salvarMensagem(){
+    var formularioNome = document.getElementById('formularioNome')
+    var formularioSobrenome = document.getElementById('formularioSobrenome')
+    var formularioEmail = document.getElementById('formularioEmail')
+    var formularioMensagem = document.getElementById('formularioMensagem')
+
+    var dados = JSON.parse(localStorage.getItem('dadosMensagem'))
+
+    if(
+        formularioNome.value.trim() === '' || formularioSobrenome.value.trim() === '' || 
+        formularioEmail.value.trim() === '' || formularioMensagem.value.trim() === ''
+    )
+
+    if(dados === null){;
+        dados = [];
+    }
+
+    var registrar = {
+        nome: formularioNome.value,
+        sobrenome: formularioSobrenome.value,
+        email: formularioEmail.value,
+        mensagem: formularioMensagem.value
+    }
+
+    dados.push(registrar);
+    localStorage.setItem('dadosMensagem',JSON.stringify(dados));
+
+    formularioNome.value ='';
+    formularioSobrenome.value = '';
+    formularioEmail.value = '';
+    formularioMensagem.vlaue = '';
+
+    alert('Mensangem enviada com sucesso!')
+}
